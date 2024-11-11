61636da724b284af2998bad0554735b7af68dc17
madmax <maksimkholin@gmail.com>
2024-11-11 17:57:35 +0300

    change domain entity

--- app/src/main/java/com/example/pussies/domain/Pussy.kt
+++ app/src/main/java/com/example/pussies/domain/Pussy.kt
@@ -1,56 +1,27 @@
 package com.example.pussies.domain
 
 data class Pussy(
-    val breeds: List<Breed>,
-    val id: String,
-    val url: String,
-    val width: Int,
-    val height: Int
-)
-
-data class Breed(
-    val weight: Weight,
-    val id: String,
-    val name: String,
-    val cfa_url: String,
-    val vetstreet_url: String,
-    val vcahospitals_url: String,
+    val imageUrl: String,
+    val weight: String,
+    val breedName: String,
     val temperament: String,
     val origin: String,
-    val country_codes: String,
-    val country_code: String,
     val description: String,
-    val life_span: String,
-    val indoor: Int,
-    val lap: Int,
-    val adaptability: Int,
-    val affection_level: Int,
-    val child_friendly: Int,
-    val cat_friendly: Int,
-    val dog_friendly: Int,
-    val energy_level: Int,
-    val grooming: Int,
-    val health_issues: Int,
-    val intelligence: Int,
-    val shedding_level: Int,
-    val social_needs: Int,
-    val stranger_friendly: Int,
-    val vocalisation: Int,
-    val bidability: Int,
-    val experimental: Int,
-    val hairless: Int,
-    val natural: Int,
-    val rare: Int,
-    val rex: Int,
-    val suppressed_tail: Int,
-    val short_legs: Int,
-    val wikipedia_url: String,
-    val hypoallergenic: Int,
-    val reference_image_id: String
+    val lifeSpan: String,
+    val indoor: String,
+    val adaptability: String,
+    val affectionLevel: String,
+    val childFriendly: String,
+    val catFriendly: String,
+    val dogFriendly: String,
+    val energyLevel: String,
+    val healthIssues: String,
+    val intelligence: String,
+    val socialNeeds: String,
+    val strangerFriendly: String,
+    val vocalisation: String,
+    val wikipediaUrl: String,
+    val hypoallergenic: String
 )
 
-data class Weight(
-    val imperial: String,
-    val metric: String
-)
 
