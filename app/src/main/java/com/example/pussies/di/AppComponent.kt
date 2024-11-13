1805e15f508d38d47793d12ff0a846d3a59890ff
madmax <maksimkholin@gmail.com>
2024-11-13 12:23:35 +0300

    add: Configure Dagger Component Factory with Application binding

--- app/src/main/java/com/example/pussies/di/AppComponent.kt
+++ app/src/main/java/com/example/pussies/di/AppComponent.kt
@@ -1,16 +1,23 @@
 package com.example.pussies.di
 
+import android.app.Application
 import com.example.pussies.presentation.MainActivity
+import dagger.BindsInstance
 import dagger.Component
 
 @Component(
     modules = [
         DatabaseModule::class,
         NetworkModule::class,
+        RepositoryModule::class,
         ViewModelModule::class
     ]
 )
 interface AppComponent {
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance application: Application): AppComponent
+    }
 
     fun inject(activity: MainActivity)
 }
