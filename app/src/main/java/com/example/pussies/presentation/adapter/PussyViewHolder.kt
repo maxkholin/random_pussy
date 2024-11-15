1fcd1b01ff4a23b9f938610e569e42771b25d624
madmax <maksimkholin@gmail.com>
2024-11-15 17:14:26 +0300

    add PussyViewHolder

---
+++ app/src/main/java/com/example/pussies/presentation/adapter/PussyViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.pussies.presentation.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pussies.databinding.ItemFavoritePussyBinding
+
+class PussyViewHolder(
+    val binding: ItemFavoritePussyBinding
+) :
+    RecyclerView.ViewHolder(binding.root) {
+}
