cc589aa7b38444d6d7c8374e89af30e95c744e12
madmax <maksimkholin@gmail.com>
2024-11-21 00:50:40 +0300

    refactor: rename values

---
+++ app/src/main/java/com/example/pussies/favorites/adapter/PussyViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.pussies.favorites.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pussies.databinding.CardPussyBinding
+
+class PussyViewHolder(
+    val binding: CardPussyBinding
+) :
+    RecyclerView.ViewHolder(binding.root) {
+}
