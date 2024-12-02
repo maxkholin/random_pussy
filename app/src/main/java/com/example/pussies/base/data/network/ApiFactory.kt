b7b71a2ae19bd7b576ba9e5d486b9d69f4f4ff80
madmax <maksimkholin@gmail.com>
2024-12-02 14:29:54 +0300

    add http timeout

--- app/src/main/java/com/example/pussies/base/data/network/ApiFactory.kt
+++ app/src/main/java/com/example/pussies/base/data/network/ApiFactory.kt
@@ -1,14 +1,24 @@
 package com.example.pussies.base.data.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiFactory {
     private const val BASE_URL = "https://api.thecatapi.com/v1/images/"
 
     val instance: ApiService by lazy {
+
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.SECONDS)
+            .writeTimeout(5, TimeUnit.SECONDS)
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
--- app/src/main/java/com/example/pussies/base/di/NetworkModule.kt
+++ app/src/main/java/com/example/pussies/base/di/NetworkModule.kt
@@ -3,8 +3,10 @@ package com.example.pussies.base.di
 import com.example.pussies.base.data.network.ApiService
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 @Module
 object NetworkModule {
@@ -12,9 +14,19 @@ object NetworkModule {
     private const val BASE_URL = "https://api.thecatapi.com/v1/images/"
 
     @Provides
-    fun provideRetrofit(): Retrofit {
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(3, TimeUnit.SECONDS) // Таймаут подключения
+            .readTimeout(3, TimeUnit.SECONDS)    // Таймаут чтения
+            .writeTimeout(3, TimeUnit.SECONDS)   // Таймаут записи
+            .build()
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
