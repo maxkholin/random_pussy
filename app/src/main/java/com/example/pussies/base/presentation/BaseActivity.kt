bbcfd6d68cfe655c6ec4846e9783b1f42efee6e9
madmax <maksimkholin@gmail.com>
2024-12-02 12:30:36 +0300

    rename base activity

--- app/src/main/java/com/example/pussies/base/presentation/BaseActivity.kt
+++ app/src/main/java/com/example/pussies/base/presentation/BaseActivity.kt
@@ -7,14 +7,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.pussies.R
 
-class MainActivity : AppCompatActivity() {
+class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.base)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_container)) { v, insets ->
+        setContentView(R.layout.base_container)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.base_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
