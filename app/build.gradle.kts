3b73c5688d6bf5388e5cb603cb544b3ebc3dd14c
madmax <maksimkholin@gmail.com>
2024-12-04 13:34:18 +0300

    add test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,10 @@ dependencies {
 
     implementation (libs.skeletonlayout)
 
-    testImplementation(libs.junit)
+    testImplementation (libs.junit)
+    testImplementation (libs.mockk)
+    testImplementation (libs.kotlinx.coroutines.test)
+    testImplementation (libs.androidx.core.testing)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
