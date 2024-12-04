88a5323f7e49d721d887d65813c3dce2530f97bb
madmax <maksimkholin@gmail.com>
2024-12-04 13:37:38 +0300

    change breedInfo now List instead ArrayList

--- app/src/main/java/com/example/pussies/base/data/network/model/PussyDto.kt
+++ app/src/main/java/com/example/pussies/base/data/network/model/PussyDto.kt
@@ -3,7 +3,7 @@ package com.example.pussies.base.data.network.model
 import com.google.gson.annotations.SerializedName
 
 data class PussyDto(
-    @SerializedName("breeds") val breedInfo: ArrayList<BreedInfoDto>,
+    @SerializedName("breeds") val breedInfo: List<BreedInfoDto>,
     @SerializedName("id") val id: String,
     @SerializedName("url") val url: String
 )
